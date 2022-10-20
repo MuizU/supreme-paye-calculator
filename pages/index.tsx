4d1c3eceb3d24b408abff7f3a3acd8b99a09dd33
Muiz Uvais <35157872+MuizU@users.noreply.github.com>
2022-10-20 22:13:00 +0530

    Created form with table summary (#3)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,84 @@
 import Head from "next/head";
 import React from "react";
 import styles from "../styles/Home.module.css";
-import nextPackage from "package.json";
+import DataTable from "react-data-table-component";
 
 export default function Home({}) {
+  const calculate = () => {};
+  const columns = [
+    {
+      name: "Title",
+      selector: (row) => row.title,
+    },
+    {
+      name: "Year",
+      selector: (row) => row.year,
+    },
+  ];
+
+  const data = [
+    {
+      id: 1,
+      title: "Beetlejuice",
+      year: "1988",
+    },
+    {
+      id: 2,
+      title: "Ghostbusters",
+      year: "1984",
+    },
+  ];
+
   return (
     <div className={styles.container}>
       <Head>
         <title>PAYE Calculator</title>
         <meta name="description" content="Sri Lanka Tax Calculator" />
-        <meta name="keywords" content="PAYE, Sri Lanka, Taxes, Taxation, Sri, Lanka, Tax, Sri Lankan Tax, Sri Lankan Taxes" />
+        <meta
+          name="keywords"
+          content="PAYE, Sri Lanka, Taxes, Taxation, Sri, Lanka, Tax, Sri Lankan Tax, Sri Lankan Taxes"
+        />
         <meta name="author" content="Muiz Uvais" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a> v
-          {nextPackage.version}
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
+      <main>
+        <div className="row">
+          <h1>PAYE Calculator</h1>
+        </div>
+        <div className="row">
+          <form onSubmit={calculate}>
+            <div className="form-group">
+              <label htmlFor="income">Monthly Income</label>
+              <input type="number" className="form-control" id="income" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="income-type">Income Type</label>
+              <select
+                id="income-type"
+                className="form-control"
+                title="Income Type"
+              >
+                <option value="annual">Annual</option>
+                <option value="monthly">Monthly</option>
+              </select>
+            </div>
+          </form>
+        </div>
+        <div className="mt-5 row">
+          <table className="table table-bordered">
+            <tbody>
+              <tr>
+                <th>Taxes to be paid</th>
+                <td>LKR 100,000</td>
+              </tr>
+              <tr>
+                <th>Income after taxes</th>
+                <td>LKR 10,000,000</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </main>
     </div>
   );

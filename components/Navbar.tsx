7a0d43b3c3da68528cd89141a1598a7a32028a50
MuizU <muizuvais99@gmail.com>
2022-10-23 23:36:56 +0530

    Added navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-dark bg-dark mb-5">
+      <a href="#" className="navbar-brand mb-0 h1 mx-auto">Supreme PAYE Calculator</a>
+    </nav>
+  );
+}

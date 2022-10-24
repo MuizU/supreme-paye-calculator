9d7d9ca217fdc05d97af683e6a7b30241b351e43
MuizU <muizuvais99@gmail.com>
2022-10-24 12:51:25 +0530

    Added header

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,11 @@
+import Image from 'next/image';
+import Logo from '../assets/logo.png';
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-dark bg-dark mb-5">
-      <a href="#" className="navbar-brand mb-0 h1 mx-auto">Supreme PAYE Calculator</a>
+    <nav className="navbar navbar-dark navbar-expand-lg bg-dark mb-5">
+      <a href="#" className="navbar-brand">
+      <img src={Logo} alt="logo" width={'60px'} height={'60px'} />
+      </a>
     </nav>
   );
 }

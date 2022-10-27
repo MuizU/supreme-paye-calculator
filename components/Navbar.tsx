348ee8573130f70d4845561879ea4b1eb4d5b952
MuizU <muizuvais99@gmail.com>
2022-10-27 12:15:55 +0530

    Created navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,12 @@
-import Image from 'next/image';
-import Logo from '../assets/logo.png';
+import Image from "next/image";
+import Logo from "../assets/logo.png";
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-dark navbar-expand-lg bg-dark mb-5">
-      <a href="#" className="navbar-brand">
-      <img src={Logo} alt="logo" width={'60px'} height={'60px'} />
+    <nav className="navbar navbar-dark bg-dark mb-5">
+      <a href="#" className="navbar-brand mb-0 ms-2 mt-0 ">
+        <Image src={Logo} alt="logo" width={40} height={40} />
       </a>
+        <span className="navbar-brand mb-0 h1 mx-auto"> Supreme PAYE Calculator</span>
     </nav>
   );
 }

c065306ae3e662c60d759fb47bdefcdf7b1a19bc
MuizU <muizuvais99@gmail.com>
2022-11-01 23:45:44 +0530

    Added footer

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="text-center text-lg-start bg-light text-muted">
+      <div
+        className="text-center p-4"
+        style={{ backgroundColor: "rgba(0,0,0, 0.05)" }}
+      ></div>
+      © 2022 Copyright;
+      <a href="https://muiz-uvais.ml" className="text-reset fw-bold">
+        Muiz Uvais
+      </a>
+    </footer>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "components/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer/>
+    </>
+  );
 }
 
 export default MyApp;

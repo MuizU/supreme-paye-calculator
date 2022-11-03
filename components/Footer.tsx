710e201ad26e650bd1ba4cf66717807df457fd28
Muiz Uvais <35157872+MuizU@users.noreply.github.com>
2022-11-03 16:52:57 +0530

    Feat/footer (#6)
    * Added footer
    
    * Created Layout component
    
    * Fixed footer text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,15 @@
-export default function Footer(){
-    
+export default function Footer() {
+  return (
+    <footer className="text-center text-lg-start bg-light text-muted">
+      <div
+        className="text-center p-4"
+        style={{ backgroundColor: "rgba(0,0,0, 0.05)" }}
+      >
+        © 2022 Copyright;&nbsp;
+        <a href="https://muiz-uvais.ml" className="text-reset fw-bold">
+          Muiz Uvais
+        </a>
+      </div>
+    </footer>
+  );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,12 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "../components/Navbar";
+import Layout from "components/Layout";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Navbar />
+    <Layout>
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }
 
